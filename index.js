29b8bdfcdec038cd4a6ebea0e911466dbcdd0c60
Marcel-mosha <marcellmosha84@gmail.com>
2025-03-31 13:19:56 +0300

    Add database to the simple blog

--- index.js
+++ index.js
@@ -1,67 +1,60 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 4000;
-
-// In-memory data store
-let posts = [
-  {
-    id: 1,
-    title: "The Rise of Decentralized Finance",
-    content:
-      "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
-    author: "Alex Thompson",
-    date: "2023-08-01T10:00:00Z",
-  },
-  {
-    id: 2,
-    title: "The Impact of Artificial Intelligence on Modern Businesses",
-    content:
-      "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
-    author: "Mia Williams",
-    date: "2023-08-05T14:30:00Z",
-  },
-  {
-    id: 3,
-    title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
-    content:
-      "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
-    author: "Samuel Green",
-    date: "2023-08-10T09:15:00Z",
-  },
-];
-
-let lastId = 3;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//CHALLENGE 1: GET All posts
-app.get("/posts", (req, res) => {
+dotenv.config();
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
+async function getPosts() {
+  const result = await db.query("SELECT * FROM blog_posts");
+  return result.rows;
+}
+const posts = await getPosts();
+//GET All posts
+app.get("/posts", async (req, res) => {
   res.json(posts);
 });
-//CHALLENGE 2: GET a specific post by id
-app.get("/posts/:id", (req,res) => {
+//GET a specific post by id
+app.get("/posts/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const selectedPost = posts.find((post) => post.id === id);
   res.json(selectedPost);
 });
-//CHALLENGE 3: POST a new post
-app.post("/posts", (req, res) => {
+//POST a new post
+app.post("/posts", async (req, res) => {
   const newPost = {
     id: posts.length + 1,
     title: req.body.title,
     content: req.body.content,
     author: req.body.author,
-    date: new Date()
+    date: new Date(),
   };
   posts.push(newPost);
+  await db.query(
+    "INSERT INTO blog_posts (title, content, author) VALUES ($1, $2, $3);",
+    [newPost.title, newPost.content, newPost.author]
+  );
   res.json(newPost);
 });
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
-app.patch("/posts/:id", (req, res) => {
+// PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const currentPost = posts.find((post) => post.id === id);
   const updatedPost = {
@@ -69,18 +62,23 @@ app.patch("/posts/:id", (req, res) => {
     title: req.body.title || currentPost.title,
     content: req.body.content || currentPost.content,
     author: req.body.author || currentPost.author,
-    date: new Date()
+    date: new Date(),
   };
   const searchIndex = posts.findIndex((post) => post.id === id);
   posts[searchIndex] = updatedPost;
+  await db.query(
+    `UPDATE blog_posts SET title = $1, content = $2, author = $3 WHERE id = $4 RETURNING *`,
+    [updatedPost.title, updatedPost.content, updatedPost.author, id]
+  );
   res.json(updatedPost);
 });
 //CHALLENGE 5: DELETE a specific post by providing the post id.
-app.delete("/posts/:id", (req, res) => {
+app.delete("/posts/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const searchIndex = posts.findIndex((post) => post.id === id);
   if (searchIndex > -1) {
     posts.splice(searchIndex, 1);
+    await db.query("DELETE FROM blog_posts WHERE id = $1", [id]);
     res.sendStatus(200);
   } else {
     res
